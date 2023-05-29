c2ecb8828ce87f20083e64aac343bd55c7ddef70
Arjunan K <arjunank680620@gmail.com>
2023-05-29 20:46:56 +0530

    Connect MongoDB Atlas

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
   providers: [
@@ -9,7 +11,27 @@ const handler = NextAuth({
     }),
   ],
   async session({ session }) {},
-  async signIn({ profile }) {},
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+
+      const userExists = await User.findOne({email: profile.email})
+
+      if(!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture
+
+        })
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
 
 export { handler as Get, handler as Post };
