9ea86f00532e317565db25505f295925999074a6
Arjunan K <arjunank680620@gmail.com>
2023-05-31 23:21:37 +0530

    Create prompt search component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,31 @@
-const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+"use client";
 
-export default Feed
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const Feed = () => {
+  const [searchText, setSearchText] = useState("")
+
+  const handleSearchChange = (e) => {
+
+  }
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={[]} handleTagClick={() => {}}/>
+    </section>
+  );
+};
+
+export default Feed;
